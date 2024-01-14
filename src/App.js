edf78bbeca9b94dee406e69621fc54b661158627
MRNSJ26 <jijjinoor@gmail.com>
2024-01-14 05:58:36 +0300

    Used State Lifting to allow two sibling components to use ItemsState + made the displayed items list local

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -5,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stat />
     </div>
   );
@@ -19,35 +27,56 @@ function Logo() {
   return <h1>🏝 Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
+  //setting the state of the element to react
+  const [description, setDiscription] = useState("");
+  const [quantity, setquantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    onAddItems(newItem);
+
+    setDiscription("");
+    setquantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip 😍</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setquantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key="{num}">
+          <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      {/*setting the state of the element to react - by adding value*/}
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        // In here we are allowing the change of the value to be displayed and controlled
+        onChange={(e) => setDiscription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => {
-          return <Item item={item} key="{item.id}" />;
-        })}
-        <li></li>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
       </ul>
     </div>
   );
